00bdadf8cb98fcbf6a31cd4ec2d31084d8f75732
Didarul Islam <didar200@gmail.com>
2021-06-20 16:17:54 +0600

    Login page and Login Controller created

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'RGL Ticketing System'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Dhaka',
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:MQzRQYGkiyJqs/PfgTvK20Buy+aX6lE2R66Lf4dwiPs='),
 
     'cipher' => 'AES-256-CBC',
 
